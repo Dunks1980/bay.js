e1803c91784f35632db27f7741056e26080e6ad6
Ian Dunkerley <Dunks1980@users.noreply.github.com>
2023-01-15 12:30:09 +0000

    Fix for if crypto.randomUUID not a function.

--- bay.mjs
+++ bay.mjs
@@ -97,7 +97,16 @@ const bay = () => {
    * @param {number} length length of the string to be generated
    */
   function makeid(length) {
-    return crypto.randomUUID().replaceAll("-", "").substring(0, length);
+    if (typeof crypto.randomUUID === "function") {
+      return crypto.randomUUID().replaceAll("-", "").substring(0, length);
+    } else {
+      let uuidv4 = () => {
+        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+          (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+      }
+      return uuidv4().replaceAll("-", "").substring(0, length);
+    }
   }
   // ------------------------------
 
